5dc79aa5c7b5268d6ea8642f0ee04c1421ccbb03
kg1018 <kg1018@DESKTOP-7JGQM62>
2018-04-25 14:48:28 +0800

    sss

--- MyGit/src/cn/itcast/TestGit.java
+++ MyGit/src/cn/itcast/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
   public static void main(String[] args) {
 	System.out.println("xxxx");
 	System.out.println("xxxx1");
+	System.out.println("kg1018 <kg1018@DESKTOP-7JGQM62>");
 }
 }
